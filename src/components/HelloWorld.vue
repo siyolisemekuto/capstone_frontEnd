6dcd03b6e4ff1c26c8550639bf292329de792f0f
siyolisemekuto <siyozamekuto2.0@gmail.com>
2022-09-02 16:00:49 +0200

    commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,11 @@
   <div class="hello">
     <div class="container">
    <img :src="imgURL"/>
+   <div class="links">
    <router-link :to="{name:'register'}" class="register">Register an account</router-link>
    <router-link :to="{name:'login'}" class="register">login to your account</router-link>
   </div>
+</div>
   </div>   
 </template>
 
@@ -13,7 +15,7 @@ export default {
    name:"HelloWorld",
    data () {
     return{
-      imgURL: "https://i.postimg.cc/Hkcdkf6M/logo-removebg-preview.png"
+      imgURL: "https://i.postimg.cc/3xwf5cXD/logo-removebg-preview-1.png"
     }
   }
 }
@@ -21,7 +23,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.container{
-  
+.container img{
+  height:50%;
+  width:50%;
+}
+.register{
+  display:block
 }
 </style>
