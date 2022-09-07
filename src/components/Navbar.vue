99742041a4e0bf866073327e8de3d4dfb8282a8d
siyolisemekuto <siyozamekuto2.0@gmail.com>
2022-09-07 10:04:12 +0200

    fix navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,15 @@
 <template>
     <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid collapse navbar-collapse">
-                <button @click="showNav()" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <i class=" navbar-toggler-icon"></i>
-                </button>
-                <div class="navbar-nav">
-                    <Profile/>
-                    <router-link class="nav-link active" :to="{name:'logmood'}">Log a mood</router-link>
-                    <router-link class="nav-link" :to="{name:'record'}">Mood Trend</router-link>
-                    <router-link class="nav-link" :to="{name:'about'}">How this app works</router-link>
-                    <a href="" download>download record in pdf</a>
-                </div>
-            </div>
-        </nav>
+        <div id="mySidenav" class="sidenav">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <Profile/>
+            <router-link class="nav-link active" :to="{name:'logmood'}">Log a mood</router-link>
+            <router-link class="nav-link" :to="{name:'record'}">Mood Trend</router-link>
+            <router-link class="nav-link" :to="{name:'about'}">How this app works</router-link>
+            <a href="https://capstone-mood-tracker.herokuapp.com/users/${payload.id}/${payload.id}/view-all" 
+            :file="file">download record in pdf</a>
+        </div>
+        <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
     </div>
 </template>
 <script>
@@ -23,16 +19,20 @@ export default {
     components:{
         Profile
     },
-    methods:{
-    showNav(){
-      this.Navbar = !this.Navbar
-    }
+    method:{
+         openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+    },
+
+        closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
 }
 
 </script>
 <style>
-    .navbar-nav {
+    /* .navbar-nav {
   margin:0;
   padding: 0;
   width: 60px;
@@ -80,5 +80,43 @@ nav ul li a:hover::after{
 }
 .router-link-exact-active {
   color: #6495ED !important;
-} 
+}  */
+.sidenav {
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  transition: 0.5s;
+  padding-top: 60px;
+}
+
+.sidenav router-link {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+}
+
+.sidenav router-link:hover {
+  color: #f1f1f1;
+}
+
+.sidenav .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+}
 </style>
